49b99bad0ae78a627d88bd46252f719fd2ef3e91
Leonid Dubershtein <ldubersht@gmail.com>
2023-06-27 19:11:06 +0300

    revert tests

--- week-4/day-1/Callback_Functions/scripts.js
+++ week-4/day-1/Callback_Functions/scripts.js
@@ -1,27 +1,21 @@
 
-// let users = []
+let users = []
 
-// const getData = function (callback) {
-//     setTimeout(function () {
-//         users = [{ name: "Rick" }, { name: "Morty" }]
-//         console.log("Got users")
-//         callback()
-//     }, 3000);
-// }
-
-// const displayData = function () {
-//     console.log("Going to display: " + users)
-//     for (user of users) {
-//         console.log(user.name)
-//     }
-// }
-
-// getData(displayData)
-// displayData()
-
-const run = (name) => {console.log("Run, " + name + ", run!")}
-const yell = function(action){
-    let env = "Forest"
-    action(env)
+const getData = function (callback) {
+    setTimeout(function () {
+        users = [{ name: "Rick" }, { name: "Morty" }]
+        console.log("Got users")
+        callback()
+    }, 3000);
 }
-run(yell)
+
+const displayData = function () {
+    console.log("Going to display: " + users)
+    for (user of users) {
+        console.log(user.name)
+    }
+}
+
+getData(displayData)
+displayData()
+
